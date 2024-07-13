6343119836ae4bb413996ad1a0043f6ec29ed979
guiwitz <guiwitz@users.noreply.github.com>
2024-07-14 01:39:49 +0200

    add acquisition date

--- software/control/towbin_funs.py
+++ software/control/towbin_funs.py
@@ -1,3 +1,4 @@
+from datetime import datetime, date
 from ome_types.model import OME, Image, Pixels
 import tifffile
 from control._def import *
@@ -11,9 +12,12 @@ def save_single_plane_tiff(image, saving_path, dtype='uint8',
     # "µm" doesn't work because of an encoding issue, but it is default anyway
     #physical_size_x_unit = 'micron \xB5'.encode('latin-1')
     #physical_size_y_unit = 'micron \xB5'.encode('latin-1')#"µm"
+
+    iso_date_time = datetime.now().isoformat()
     
     ome = OME()
     img = Image(
+        acquisition_date=iso_date_time,
         pixels=Pixels(
             size_c=1, size_t=1, size_x=image.shape[1], size_y=image.shape[0], size_z=1,
             type=dtype, dimension_order='XYZCT',
