8a4c16553938669d959c955d3d03fd3d4253ffa8
guiwitz <guiwitz@users.noreply.github.com>
2025-04-10 09:58:45 +0200

    fix custom ui

--- software/control/towbin_widget.py
+++ software/control/towbin_widget.py
@@ -97,18 +97,20 @@ class TowbinWidget(QWidget):
         self.layout.addLayout(grid_channels)
         self.check_save_multichannel = QCheckBox("Save multichannel")
         self.check_save_multichannel.setChecked(True)
-        grid_channels.addWidget(self.check_save_multichannel,2,1)
+        grid_channels.addWidget(self.check_save_multichannel,0,1)
 
+        grid_custom = QGridLayout()
+        self.layout.addLayout(grid_custom)
         self.btn_select_custom = QPushButton("Custom multipoint")
         self.btn_select_custom.setDefault(False)
         #self.btn_select_custom.setIcon(QIcon("icon/folder.png"))
-        grid_crop.addWidget(self.btn_select_custom,3,1)
+        grid_custom.addWidget(self.btn_select_custom,0,1)
         self.btn_select_custom.clicked.connect(self.set_custom_file)
 
         self.lineEdit_customfile = QLineEdit()
         self.lineEdit_customfile.setReadOnly(True)
         self.lineEdit_customfile.setText("Choose a custom file")
-        grid_crop.addWidget(self.lineEdit_customfile,3,2)
+        grid_custom.addWidget(self.lineEdit_customfile,0,2)
 
         self.parent.destroyed.connect(self.close_widget)
 
